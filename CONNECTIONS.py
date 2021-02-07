62d7a0a7ca9148c598b004b1e11e788ffffb286d
Greg Norris <gregn913@gmail.com>
2021-02-07 07:46:35 -0700

    Restructuring IRCCON

--- CONNECTIONS.py
+++ CONNECTIONS.py
@@ -1,7 +1,8 @@
 import asyncio
 import ssl
 from collections import deque
-
+from IRCCON_OLD_ASYC import IRCCON as IRC
+from Queues import MessageQueues
 '''
 *Asynchronous connections over SSL. Supports IRC client operations.
 *Shares a messageQueues class to transport data between IRC and bot client.
@@ -12,37 +13,14 @@ Currently uses hard-coded values for development.
 
 
 # Used to send data between IRCCON and Sonderbot
-# Probably switch to Pandas DF in future update.
-
-class MessageQueues:
-    queue = {
-        "hostname": {
-            'incomming': deque(),
-            'outgoing': deque(),
-            'commands': deque({'default_command': 'default params'})
-        }
-    }
-
-    @property
-    def __str__(self):
-        output = ''
-        for msg in self.queue:
-            output = output + "\n" + msg
-        return output
 
 
 class Connection:
-    def __init__(self, hostname=None, port=None,
-                 botnick="SonderBot", botnick2="Biggus",
-                 botnick3="Maximus", botpass="SonderbotPW",
-                 messagequeue=MessageQueues):
-        self.hostname = hostname
-        self.port = port
-        self.botnick = botnick
-        self.botnick2 = botnick2
-        self.botnick3 = botnick3
-        self.botpass = botpass
-        self.messagequeue = messagequeue
+    def __init__(self, contype, **kwargs):
+        irc_req = ['contype','ssl', 'hostname', 'port', 'botnick',
+               'botnick1', 'botnick2', 'botnick3', 'botpass']
+        discord_req = []
+        asyncio.create_task(IRC(kwargs))
 
 
 class SBConnections:
